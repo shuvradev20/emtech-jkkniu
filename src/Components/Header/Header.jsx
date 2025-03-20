04d9659f10b721230cd6b11f1251baf91e36cf14
shuvradev20 <shuvra1149131@gmail.com>
2025-03-20 15:08:19 +0600

    code edited

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBars, faX} from '@fortawesome/free-solid-svg-icons'
+import {faX} from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
 
